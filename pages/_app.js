a91c382364c27a8356a208ea3f4b45acb4dbc6ac
Jason Jun <uxd.jun@gmail.com>
2022-01-10 14:20:29 +1100

    Basic setup

--- pages/_app.js
+++ pages/_app.js
@@ -3,7 +3,19 @@ import { globalCss } from "@stitches/react"
 
 const globalStyles = globalCss({
   body: {
-    fontFamily: "sans-serif",
+    fontFamily: `"IBM Plex Sans", sans-serif`,
+  },
+  "@font-face": {
+    fontFamily: "Inter",
+    fontDisplay: "swap",
+    src: 'url("/fonts/inter-var-latin.woff2") format("woff2-variations")',
+    fontWeight: "1 999",
+  },
+  "@font-face": {
+    fontFamily: "IBM Plex Sans",
+    fontDisplay: "swap",
+    src: 'url("/fonts/ibm-plex-sans-var-roman.woff2") format("woff2-variations")',
+    fontWeight: "1 999",
   },
 })
 
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,13 @@
+import Header from "@components/Header"
+import { Box } from "src/styles/style"
+
 export default function Home() {
   return (
     <>
-      <div>test</div>
+      <Header />
+      <Box fullWidth css={{ marginTop: "48px" }}>
+        <Box>Donec sed odio dui.</Box>
+      </Box>
     </>
   )
 }
